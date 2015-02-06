feb2ee91cdec75e47594d7a7b1c044f17dfa741d
irvin1000 <i_abellanosa@yahoo.com>
2015-02-06 23:43:19 +0800

    only lacking resize image and alert

--- PBA/application/views/Content/admin_products.php
+++ PBA/application/views/Content/admin_products.php
@@ -28,7 +28,7 @@ $(document).ready(function(){
   var users="";
   <?php foreach($products as $product):
     $user=$this->account_model->get_user2($product->USER_ID);?>
-    users+="<tr><input type='hidden' class='prod_id' value='<?php echo $product->PROD_ID;?>'/><td class='table-content hide'><?php echo $product->PROD_ID;?></td><td class='table-content productName' onclick=\"window.location ='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'\"><img src='<?php  echo $product->IMAGE1;?>' width='70'/><br/><?php echo $product->PROD_NAME;?></td><td class='table-content' id='username'><img src='<?php echo $user->USER_IMAGE;?>' width='70'/><br/><?php echo $user->USERNAME;?></td><td class='table-content' id='pType'><?php echo $product->PROD_CAT;?></td><td class='table-content' id='pStatus'><?php echo $product->PROD_STAT;?></td><td><?php echo $product->START_BID;?></td><td><button class='button alert tiny del'>X</button></td></tr>";
+    users+="<tr><input type='hidden' class='prod_id' value='<?php echo $product->PROD_ID;?>'/><td class='table-content hide'><?php echo $product->PROD_ID;?></td><td class='table-content productName' onclick=\"window.location ='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'\"><img src='<?php  echo $product->IMAGE1;?>' width='70'/><br/><?php echo $product->PROD_NAME;?></td><td class='table-content' id='username'><img src='<?php echo $user->USER_IMAGE;?>' width='70'/><br/><?php echo $user->USERNAME;?></td><td class='table-content pType'><?php echo $product->PROD_CAT;?></td><td class='table-content pStatus'><?php echo $product->PROD_STAT;?></td><td><?php echo $product->START_BID;?></td><td><button class='button alert tiny del'>X</button></td></tr>";
   <?php endforeach;?>
   $("#tbody").append(users);
   $('#searchTable').dataTable({
@@ -36,16 +36,30 @@ $(document).ready(function(){
   });
 
   $("#tbody").on('click','.del',function(){
-    if(confirm("Delete Product '"+$(this).parent().siblings(".productName").text()+"' ?") == true) {
-      $.ajax({
-        url:'<?php echo base_url();?>auction_controller/delProductAdmin/',
-        type:'post',
-        data:{'id':$(this).parent().siblings(".prod_id").val()},
-        success:function(data,status){
-          alert(data);
-          window.location.reload(true);
-        }
-      });
+    if($(this).parent().siblings(".pStatus").text()!='On-going'){
+      if(confirm("Delete Product '"+$(this).parent().siblings(".productName").text()+"' ?") == true) {
+        $.ajax({
+          url:'<?php echo base_url();?>auction_controller/delProductAdmin/',
+          type:'post',
+          data:{'id':$(this).parent().siblings(".prod_id").val()},
+          success:function(data,status){
+            alert(data);
+            window.location.reload(true);
+          }
+        });
+      }
+    }else{
+      if(confirm("Caution: This is product is still in auction, delete product '"+$(this).parent().siblings(".productName").text()+"' ?") == true) {
+        $.ajax({
+          url:'<?php echo base_url();?>auction_controller/delProductAdmin/',
+          type:'post',
+          data:{'id':$(this).parent().siblings(".prod_id").val()},
+          success:function(data,status){
+            alert(data);
+            window.location.reload(true);
+          }
+        });
+      }
     }
   });
 });
