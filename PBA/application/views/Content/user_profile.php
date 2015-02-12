9058ca21918066a6f8be9caccd74a56fef99fecc
Paulo <andrepaulo.dr@gmail.com>
2015-02-12 23:47:09 +0800

    UPDATED VIEW: user_profile ||| UPDATED app.css

--- PBA/application/views/Content/user_profile.php
+++ PBA/application/views/Content/user_profile.php
@@ -24,13 +24,13 @@
         <div class="right">
         <a href="#" data-reveal-id="questionProfileModal" class="radius button">Edit profile</a>
         <a href="#" data-reveal-id="passwordModal" class="radius button">Change password</a>
-    </div>
-        <div class="large-12 columns header">     
-            <h2 class="header-content">
-              <img class="header-content-img" src="<?php echo base_url();?>assets/img/basketball.png" alt="basketball" />
-              <span>Products</span>
-            </h2>
-        </div>
+      </div>
+      <div class="large-12 columns header">     
+          <h2 class="header-content">
+            <img class="header-content-img" src="<?php echo base_url();?>assets/img/basketball.png" alt="basketball" />
+            <span>Products</span>
+          </h2>
+      </div>
       </div>          
       <div class="row row-content">
         <div class="large-12 columns">
@@ -51,17 +51,17 @@
                   }
                 echo substr($product->PROD_NAME, 0,20).$append;?>
               </h2>
-              <button class="button tiny disabled round">Starting Bid:<?php echo $product->START_BID;?></button>
+              <button class="button small disabled radius"><strong>Starting bid</strong> <span class="profile-elem-bar">|</span> <?php echo $product->START_BID;?></button>
               <?php if($product->PROD_STAT=='Closed'){?>
-               <button class="button tiny round alert disabled">Status:<?php echo $product->PROD_STAT;?></button>
+               <button class="button radius alert disabled"><strong>Status</strong> <span class="profile-elem-bar">|</span> <?php echo $product->PROD_STAT;?></button>
               <?php }else if($product->PROD_STAT=='On-going'){?>
-                <button class="button tiny round success disabled">Status:<?php echo $product->PROD_STAT;?></button>
+                <button class="button radius success disabled"><strong>Status</strong> <span class="profile-elem-bar">|</span> <?php echo $product->PROD_STAT;?></button>
               <?php }else{?>
-                <button class="button tiny round disabled">Status:<?php echo $product->PROD_STAT;?></button>
+                <button class="button small radius disabled"><strong>Status</strong> <span class="profile-elem-bar">|</span> <?php echo $product->PROD_STAT;?></button>
               <?php }?>
               <div>
-                <button onclick="window.location='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'" class="button tiny">View</button>
-                <button class="button tiny alert deleteProduct" data-reveal-id="deleteProdModal">Delete</button>
+                <button onclick="window.location='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'" class="button small radius">View</button>
+                <button class="button small alert deleteProduct radius" data-reveal-id="deleteProdModal">Delete</button>
               </div>
             </div>
           <?php endforeach; echo "</div>";}?>
@@ -92,139 +92,179 @@
       <!--- END USER NOTIFICATIONS-->  
 </div>      
 
-  <!--- START QUESTON USER-->
-   <div id="questionProfileModal" class="reveal-modal small" data-reveal>
-    <h2>Answer Of Your Secret Question <?php echo $this->session->userdata('username');?>!</h2>
-    <div class="small-8 large-centered columns">
-          <center> 
-              <h5><?php echo $info->SECRET_QUESTION;?></h5> 
-              <center><span style="color:red;" id="notif"></span></center>
-              <br> 
-              <input type="password" id="answer" name="answer" placeholder="Answer" aria-label="Answer" maxlength="20" required>
-          </center>
+<!--- START QUESTON USER-->
+ <div id="questionProfileModal" class="reveal-modal medium text-center" data-reveal>
+    <h2>Please supply the correct answer to your secret question.</h2>
+    <hr class="hr-dotted">
+        <p class="secret-question form-text"><?php echo $info->SECRET_QUESTION;?></p> 
+        <div class="small-11 small-centered medium-12 medium-centered large-9 large-centered columns">
+          <p class="form-notif" id="notif"></p>
+          <input class="text-center user-profile-answer-field" type="password" id="answer" name="answer" placeholder="Answer" aria-label="Answer" maxlength="20" required />
           <div class="verify-btn">
-            <button id="answerButton" class="expand button [tiny small large]">Answer</button>
+              <button id="answerButton" class="button expand small">Submit</button>
           </div>
-    </div>
+        </div>  
     <a class="close-reveal-modal">&#215;</a>
-  </div>
-  <!--- END QUESTION USER -->
+</div>
+<!--- END QUESTION USER -->
 
 <!--- START EDIT USER-->
 <div id="editProfileModal" class="reveal-modal small" data-reveal>
-  <form id="ep" action="<?php echo base_url();?>account_controller/edit_user" method="post" enctype='multipart/form-data'>      
-    <fieldset>
-      <legend style="color:blue;">Edit Profile</legend>  
-      <div class="small-12 large-12 medium-12 columns">
+  <form id="ep" action="<?php echo base_url();?>account_controller/edit_user" method="post" enctype='multipart/form-data'>       
+      <div class="small-12 medium-12 large-12 columns form-text">
         <h2>Personal Information</h2>
-        <b>First Name: </b><input type="text" name="fname" value="<?php echo $info->FIRST_NAME;?>" aria-label="First Name" required pattern=".{4,40}" title="4 to 40 Characters" maxlength="40"/>
-        <b>Last Name: </b><input type="text" name="lname" value="<?php echo $info->LAST_NAME;?>" aria-label="Last Name" required pattern=".{4,30}" title="4 to 30 Characters" maxlength="30"/>
-        <b>Contact Number: </b><input type="text" name="contact" value="<?php echo $info->CONTACT_NUMBER;?>" aria-label="Contact Number" required pattern="[0][9][0-9]{9}" title="11 Characters" maxlength="11"/>
-        <b>Address: </b><input type="text" name="address" value="<?php echo $info->ADDRESS;?>" aria-label=" Address" required maxlength="100"/>
-        <label style="color:black;font-weight:700;font-size:16px;">Profile Photo File: (Only Accepts jpeg/jpg/png files)</label>
-        <input type="file" accept="image/jpg, image/jpeg, image/png" name="userfile" value="Change Photo" class="extend form-button button [tiny small large]"/>
-      </div>                     
-    </fieldset>
-    <div class="small-10 small-centered medium-7 medium-centered large-6 large-centered columns">  
-      <div class="btn">
-        <button type="submit" class="expand form-button button [tiny small large]">Submit</button>  
-      </div>   
-    </div>       
+        <hr class="hr-dotted" />
+        <label>
+          First name
+          <input type="text" name="fname" value="<?php echo $info->FIRST_NAME;?>" aria-label="First Name" required pattern=".{4,40}" title="4 to 40 Characters" maxlength="40"/>
+        </label>
+        <label>
+          Last name
+          <input type="text" name="lname" value="<?php echo $info->LAST_NAME;?>" aria-label="Last Name" required pattern=".{4,30}" title="4 to 30 Characters" maxlength="30"/>
+        </label>
+        <label>
+          Contact number
+          <input type="text" name="contact" value="<?php echo $info->CONTACT_NUMBER;?>" aria-label="Contact Number" required pattern="[0][9][0-9]{9}" title="11 Characters" maxlength="11"/>
+        </label>
+        <label>
+          Address
+          <input type="text" name="address" value="<?php echo $info->ADDRESS;?>" aria-label=" Address" required maxlength="100"/>
+        </label>
+
+        <label>
+          Profile photo <span>(only accepts jpeg/jpg/png files)</span>
+          <input type="file" accept="image/jpg, image/jpeg, image/png" name="userfile" value="Change Photo" class="panel-orange extend form-button button [tiny small large]"/>
+        </label>
+      </div>  
+      <hr class="hr-dotted" />                   
+      <div class="small-12 medium-12 large-12 columns">  
+        <div class="btn">
+          <button type="submit" class="expand form-button button">Submit</button>  
+        </div>   
+      </div>       
   </form>
   <a class="close-reveal-modal">&#215;</a>
 </div>
 <!--- END EDIT USER -->
 
-
-
 <!--- START CHANGE PASSWORD USER-->
-<div id="passwordModal" class="reveal-modal small small-12 medium-12 columns" data-reveal>
+<div id="passwordModal" class="reveal-modal small medium-12 medium-centered columns" data-reveal>
   <form action="<?php echo base_url();?>account_controller/edit_userPassword" method="post" enctype='multipart/form-data'>      
-    <fieldset>
-      <legend style="color:blue;">Change Password</legend>  
-      <div class="small-12 large-12 medium-12 columns signup-account-info">
+      <div class="signup-account-info">
         <h2>Account Information</h2>
-        <b>Current Password: </b><input type="password" name="oldpass"  aria-label="Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
-        <b>New Password: </b><input type="password" name="pass" id="pass" aria-label="Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
-        <b>Confirm Password: </b><input type="password" name="cpass" id="cpass" aria-label="Confirm Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
+        <hr class="hr-dotted" />
+        <label>
+          Current Password
+          <input class="text-center" type="password" name="oldpass"  aria-label="Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
+        </label>
+        <label>
+          New Password
+          <input class="text-center" type="password" name="pass" id="pass" aria-label="Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
+        </label>
+        <label>
+          Confirm Password
+          <input class="text-center" type="password" name="cpass" id="cpass" aria-label="Confirm Password" required pattern=".{4,20}" title="4 to 20 Characters" maxlength="20"/>
+        </label>
       </div>                    
-    </fieldset>
-    <div class="small-10 small-centered medium-7 medium-centered large-6 large-centered columns">  
       <div class="submit-btn">
-        <button type="submit" class="expand form-button button [tiny small large]">Submit</button>  
+        <button type="submit" class="expand form-button button">Submit</button>  
       </div>   
-    </div>       
   </form>
   <a class="close-reveal-modal">&#215;</a>
 </div>
 <!--- END CHANGE PASSWORD USER -->
 
-
 <!--- START ADD PRODUCT-->
 <div id="addProductModal" class="reveal-modal" data-reveal>
   <form id="aprod" action="<?php echo base_url();?>auction_controller/addProduct" method="post" enctype='multipart/form-data'>
-    <h2>Product Information</h2>
-    <div class="small-12 large-6 medium-6 columns">
-      <b>Product Name </b><input type="text" name="pname" required maxlength="40" pattern=".{4,40}" title="4 to 40 Characters"/>
-      <b>Description </b><textarea name="pdesc" required maxlength="200" rows="5"></textarea>
-      <b>Category </b>
-      <select name="pcat">
-        <option value="Jersey">Jersey</option>
-        <option value="Shoes">Shoes</option>
-        <option value="Shorts">Shorts</option>
-        <option value="Socks">Socks</option>
-        <option value="Accessories">Accessories</option>
-      </select>
-      
-      <div class="small-12 medium-6 large-12">
-            <div style="display:inline-flex">
-            
-                  <b>Age Of Product </b><div><input type="number" name="page" required min="1"/> </div>        
-               
-                  <div class="small-6 medium-3 large-6">
-                      <select name="pagename">
-                        <option value="Day">Day/s</option>
-                        <option value="Week">Week/s</option>
-                        <option value="Month">Month/s</option>
-                        <option value="Year">Year/s</option>
-                      </select>
-                  </div>
-            </div>
+      <h2>Product Information</h2>
+      <hr class="hr-dotted">
+      <div class="large-11 large-centered columns">
+            <ul class="small-block-grid-1 medium-block-grid-1 large-block-grid-2 form-text">
+                  <li>
+                        <label>
+                          Product Name
+                          <input type="text" name="pname" required maxlength="40" pattern=".{4,40}" title="4 to 40 Characters"/>
+                        </label>
+                        <label for="">
+                          Description
+                          <textarea name="pdesc" required maxlength="200" rows="5"></textarea>
+                        </label>
+                        
+                        <div class="small-5 medium-5 large-5 columns">
+                          <label>
+                              Product type
+                              <select name="pcat">
+                                  <option value="Jersey">Jersey</option>
+                                  <option value="Shoes">Shoes</option>
+                                  <option value="Shorts">Shorts</option>
+                                  <option value="Socks">Socks</option>
+                                  <option value="Accessories">Accessories</option>
+                              </select>
+                          </label>
+                        </div>
+                        
+                        <div class="small-7 medium-7 large-7 columns">
+                          <label>
+                              Age Of Product 
+                              <ul class="small-block-grid-2 medium-block-grid-2 large-block-grid-2">                         
+                                  <li>
+                                    <input type="number" name="page" required min="1"/>
+                                  </li>
+                                  <li class="small-block-grid-1 medium-block-grid-1 large-block-grid-1">
+                                      <select name="pagename" class="product-age-cat">
+                                        <option value="Day">day(s)</option>
+                                        <option value="Week">week(s)</option>
+                                        <option value="Month">month(s)</option>
+                                        <option value="Year">year(s)</option>
+                                      </select>
+                                  </li>                      
+                              </ul>
+                          </label>
+                        </div>
+
+                        <div class="small-8 small-centered medium-8 medium-centered large-8 large-centered columns">
+                          <label>
+                            Starting bid      
+                            <input type="number" name="pbid" required min="1"/>
+                          </label>
+                        </div>
+                  </li>
+                  <li class="text-center">                    
+                    <div class="small-12 medium-12 large-12 columns">
+                        <label>
+                            5 product images <span>(only accepts jpeg/jpg/png files)</span>                  
+                                <input type="file" name="userfile[] test1"  accept="image/jpg, image/jpeg, image/png" class="panel-orange choose-img-panel"/>
+                                <input type="file" name="userfile[] test2"  accept="image/jpg, image/jpeg, image/png" class="panel-orange choose-img-panel"/>
+                                <input type="file" name="userfile[] test3"  accept="image/jpg, image/jpeg, image/png" class="panel-orange choose-img-panel"/>
+                                <input type="file" name="userfile[] test4"  accept="image/jpg, image/jpeg, image/png" class="panel-orange choose-img-panel"/>
+                                <input type="file" name="userfile[] test5"  accept="image/jpg, image/jpeg, image/png" class="panel-orange choose-img-panel"/>
+                        </label>
+                    </div>
+                  </li>
+                </ul>  
+                <hr class="hr-dotted" />
+                <div class="small-12 large-12 medium-12 columns">
+                  <center> 
+                    <button type="submit" class="form-button button [tiny small large] add-btn">Add</button> 
+                    <button id="cancelButton" class="alert button [tiny small large] cancel-btn">Cancel</button>  
+                  </center>
+                </div>
       </div>
-    </div>
-    <div class="small-12 large-6 medium-6 columns">
-      <b>Starting Bid </b><input type="number" name="pbid" required min="1"/>
-      <label style="color:black;font-weight:700;font-size:16px;">5 Product Photo Files (Only Accepts jpeg/jpg/png files)</label>
-      <input type="file" name="userfile[] test1"  accept="image/jpg, image/jpeg, image/png" class="extend form-button button [tiny small large]"/>
-      <input type="file" name="userfile[] test2"  accept="image/jpg, image/jpeg, image/png" class="extend form-button button [tiny small large]"/>
-      <input type="file" name="userfile[] test3"  accept="image/jpg, image/jpeg, image/png" class="extend form-button button [tiny small large]"/>
-      <input type="file" name="userfile[] test4"  accept="image/jpg, image/jpeg, image/png" class="extend form-button button [tiny small large]"/>
-      <input type="file" name="userfile[] test5"  accept="image/jpg, image/jpeg, image/png" class="extend form-button button [tiny small large]"/>
-    </div>  
-    <div class="small-12 large-12 medium-12 columns">
-      <center> 
-        <button type="submit" class="form-button button [tiny small large] add-btn">Add</button> 
-        <button id="cancelButton" class="alert button [tiny small large] cancel-btn">Cancel</button>  
-      </center>
-    </div>
   </form>
   <a class="close-reveal-modal">&#215;</a>
 </div>
 <!--- END ADD PRODUCT -->
 
-  <!--- START QUESTON USER DELETE PRODUCT-->
-  <div id="deleteProdModal" class="reveal-modal tiny" data-reveal>
-    <div class="small-8 large-centered columns">
-      <input type="hidden" name="prodIdModal" id="prodIdModal"/>
-      <center> 
-        <h5>Delete Product <b id="delProdName"> </b>?</h5> 
-        <button id="yesButton" class="button tiny">Yes</button>
-        <button id="noButton" class="button tiny alert">No</button>
-      </center>
-    </div>
-    <a class="close-reveal-modal">&#215;</a>
-  </div>
-  <!--- END QUESTION USER DELETE PRODUCT-->
+<!--- START QUESTON USER DELETE PRODUCT-->
+<div id="deleteProdModal" class="reveal-modal small text-center" data-reveal>
+  <input type="hidden" name="prodIdModal" id="prodIdModal"/>
+  <p>Are you sure you want to delete <strong id="delProdName"></strong>?</p> 
+  <button id="yesButton" class="button small">Yes</button>
+  <button id="noButton" class="button small alert">No</button>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+<!--- END QUESTION USER DELETE PRODUCT-->
 
 <script>
 $(document).ready(function(){
@@ -238,7 +278,7 @@ $(document).ready(function(){
         $("#notif").html("");
         $('#editProfileModal').foundation('reveal', 'open');
       }else{
-        $("#notif").html("Wrong Answer!");
+        $("#notif").html("Invalid answer. Please try again.");
       }
     });
   });
