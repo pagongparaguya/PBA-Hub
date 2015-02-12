a335db5796129bdaf5a41bbba547f2a681bf2dd0
Paulo <andrepaulo.dr@gmail.com>
2015-02-13 02:56:06 +0800

    UPDATED VIEWS: admin_products and admin_users ||| UPDATED app.css

--- PBA/application/views/Content/admin_products.php
+++ PBA/application/views/Content/admin_products.php
@@ -1,34 +1,31 @@
-<div class="page-content list-pages">
-    <div class="small-10 small-centered medium-8 medium-centered large-12 large-centered columns">
-      <h1>PRODUCTS</h1>
-        <table id="searchTable" class="display" cellspacing="0">
-          <thead>
+</div><!-- closing tag for container -->
+<div class="small-12 medium-12 large-12 columns">
+    <div class="page-content">
+      <h1>PBA-HUB PRODUCTS</h1>
+      <hr class="hr-dotted" />  
+      <table id="searchTable" class="display" cellspacing="0">
+        <thead>
           <tr>
             <th class="hide">ID</th>
-            <th class="table-content">Product</th>
-            <th class="table-content">Owner</th>
-            <th class="table-content">Product Category</th>
-            <th class="table-content">Product Status</th>
-            <th class="table-content">Starting Bid</th>
-            <th class="table-content">Action</th>
+            <th class="admin-header text-center" width="15%">Product</th>
+            <th class="admin-header text-center">Owner</th>
+            <th class="admin-header text-center">Product Category</th>
+            <th class="admin-header text-center">Product Status</th>
+            <th class="admin-header text-center">Starting Bid</th>
+            <th class="admin-header text-center">Action</th>
           </tr>
-          </thead>
-          <tbody id="tbody">
-          </tbody>
-        </table>
+        </thead>
+        <tbody id="tbody">
+        </tbody>
+      </table>
     </div> 
 </div>
-<style>
-  .hide{
-    visibility: hidden;
-  }
-</style>
 <script>
 $(document).ready(function(){
   var users="";
   <?php foreach($products as $product):
     $user=$this->account_model->get_user2($product->USER_ID);?>
-    users+="<tr><input type='hidden' class='prod_id' value='<?php echo $product->PROD_ID;?>'/><td class='table-content hide'><?php echo $product->PROD_ID;?></td><td class='table-content productName' onclick=\"window.location ='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'\"><img src='<?php  echo $product->IMAGE1;?>' width='70'/><br/><?php echo $product->PROD_NAME;?></td><td class='table-content' id='username'><img src='<?php echo $user->USER_IMAGE;?>' width='70'/><br/><?php echo $user->USERNAME;?></td><td class='table-content pType'><?php echo $product->PROD_CAT;?></td><td class='table-content pStatus'><?php echo $product->PROD_STAT;?></td><td><?php echo $product->START_BID;?></td><td><button class='button alert tiny del'>X</button></td></tr>";
+    users+="<tr><input type='hidden' class='prod_id' value='<?php echo $product->PROD_ID;?>'/><td class='text-center hide'><?php echo $product->PROD_ID;?></td><td class='text-center productName' onclick=\"window.location ='<?php echo base_url().'auction_controller/view_product/'.$product->PROD_ID;?>'\"><img class='admin-page-img' src='<?php  echo $product->IMAGE1;?>'/><br/><?php echo $product->PROD_NAME;?></td><td class='text-center' id='username'><img class='admin-page-img' src='<?php echo $user->USER_IMAGE;?>'/><br/><?php echo $user->USERNAME;?></td><td class='text-center pType'><?php echo $product->PROD_CAT;?></td><td class='text-center pStatus'><?php echo $product->PROD_STAT;?></td><td class='text-center'><?php echo $product->START_BID;?></td><td class='text-center'><button class='button alert form-button tiny del'>X</button></td></tr>";
   <?php endforeach;?>
   $("#tbody").append(users);
   $('#searchTable').dataTable({
--- PBA/application/views/Content/admin_users.php
+++ PBA/application/views/Content/admin_users.php
@@ -1,61 +1,77 @@
-<div class="page-content list-pages">
-      <div class="small-10 small-centered medium-8 medium-centered large-12 large-centered columns">
-        <h1>USERS</h1>
-          <table id="searchTable" class="display" cellspacing="0">
-            <thead>
-            <tr>
-              <th class="table-content">Username</th>
-              <th class="table-content">Fullname</th>
-              <th class="table-content">Account Type</th>
-              <th class="table-content">Account Status</th>
-              <th class="table-content">Email Address</th>
-              <th class="table-content">Contact Number</th>
-              <th class="table-content">Action</th>
-            </tr>
-            </thead>
-            <tbody id="tbody">
-            </tbody>
-          </table>
-      </div> 
-  
-  <!--Admin Modal-->
-  <div id="adminModal" class="reveal-modal small" data-reveal>
-    <h2 id="name"></h2>
-    <hr/>
-    <div>
-      <form action="<?php echo base_url();?>account_controller/edit_otherUser" method="post">
-        <input type="hidden" name="user" id="user" value="" />
-        <ul class="team-row small-block-grid-2 medium-block-grid-2 large-block-grid-2 teams">
-          <li><span style="color:blue;text-shadow:none;">TYPE: </span>
+</div><!-- closing tag for container -->
+<div class="small-12 medium-12 large-12 columns">
+  <div class="page-content">
+    <h1>PBA-HUB USERS</h1>
+    <hr class="hr-dotted" />  
+    <table id="searchTable" class="display" cellspacing="0">
+      <thead>
+        <tr>
+          <th class="admin-header text-center">User</th>
+          <th class="admin-header text-center">Username</th>
+          <th class="admin-header text-center">Account Type</th>
+          <th class="admin-header text-center">Account Status</th>
+          <th class="admin-header text-center">Email Address</th>
+          <th class="admin-header text-center">Contact Number</th>
+          <th class="admin-header text-center">Action</th>
+        </tr>
+      </thead>
+      <tbody id="tbody">
+      </tbody>
+    </table>
+  </div>
+</div>  
+<!--Admin Modal-->
+<div id="adminModal" class="reveal-modal small" data-reveal>
+  <h2 id="name"></h2>
+  <hr class="hr-dotted" />
+  <div>
+    <form action="<?php echo base_url();?>account_controller/edit_otherUser" method="post">
+      <input type="hidden" name="user" id="user" value="" />
+      <ul class="small-block-grid-2 medium-block-grid-2 large-block-grid-2 teams">
+        <li>  
+          <label>
+            User type
             <select name="type" id="userType">
               <option id="1" value="Admin">Admin</option>
               <option id="2" value="Normal">Normal</option>
             </select>
-          </li>
-          <li><span style="color:blue;text-shadow:none;">STATUS: </span>
+          </label>
+        </li>
+        <li>
+          <label>
+            User status
             <select name="status" id="userStatus">
               <option id="3" value="Active">Active</option>
               <option id="4" value="Deleted">Deleted</option>
             </select>
-          </li>
-        </ul>
-        <button type="submit" class="button" id="change">Change</button>
-      </form>
-    </div>
-    <a class="close-reveal-modal">&#215;</a>
-  </div>  
-  <!--Admin Modal-->
+          </label>
+        </li>
+      </ul>
+      <button type="submit" class="button expand [tiny small medium]" id="change">Change</button>
+    </form>
+  </div>
+  <a class="close-reveal-modal">&#215;</a>
+</div>  
+<!--Admin Modal-->
 </div>
-
 <script>
 $(document).ready(function(){
   var users="";
   <?php foreach($users as $users):?>
-    users+="<tr><td class='table-content username' onclick=\"window.location ='<?php echo base_url().'account_controller/view_otherUser/'.$users->USER_ID;?>'\"><img src='<?php echo $users->USER_IMAGE;?>' width='70'/><br/><?php echo $users->USERNAME;?></td><td class='table-content'><?php echo $users->FIRST_NAME.' '.$users->LAST_NAME;?></td><td class='table-content aType' ><?php echo $users->ACCOUNT_TYPE;?></td><td class='table-content aStatus'><?php echo $users->STATUS;?></td><td class='table-content'><?php echo $users->EMAIL_ADDRESS;?></td><td class='table-content'><?php echo $users->CONTACT_NUMBER;?></td><td><button class='button tiny edit' data-reveal-id='adminModal'>EDIT</button></td></tr>";
+    users+="<tr><td class='text-center username' onclick=\"window.location ='<?php echo base_url().'account_controller/view_otherUser/'.$users->USER_ID;?>'\"><img class='admin-page-img' src='<?php echo $users->USER_IMAGE;?>'/><br/><?php echo $users->FIRST_NAME.' '.$users->LAST_NAME;?></td><td class='text-center'><?php echo $users->USERNAME;?></td><td class='text-center aType'><?php echo $users->ACCOUNT_TYPE;?></td><td class='text-center aStatus'><?php echo $users->STATUS;?></td><td class='text-center'><?php echo $users->EMAIL_ADDRESS;?></td><td class='text-center'><?php echo $users->CONTACT_NUMBER;?></td><td class='text-center'><button class='button tiny edit form-button' data-reveal-id='adminModal'>Update</button></td></tr>";
   <?php endforeach;?>
   $("#tbody").append(users);
   $('#searchTable').dataTable({
-    "order": [ 1, 'asc' ]
+    "order": [ 1, 'asc' ],
+    "aoColumns": [
+        null,
+        { "bSortable": false },
+        null,
+        null,
+        { "bSortable": false },
+        { "bSortable": false },
+        { "bSortable": false },
+    ]
   });
 
   $("#tbody").on('click','.edit',function(){
@@ -63,7 +79,7 @@ $(document).ready(function(){
     var type = $(this).parent().siblings(".aType").text();
     var status = $(this).parent().siblings(".aStatus").text();
     $('#user').val(name);
-    $('#name').html("Changing "+name+"'s Account Details");
+    $('#name').html(name+"'s account details");
     
     $("#userType").val(type);
     $("#userStatus").val(status);
